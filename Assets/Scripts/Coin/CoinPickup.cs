c55489196ea78587491f6de2724f73e3ab796246
Matteo Carlucci <matteo_carlucci@icloud.com>
2026-03-18 00:33:20 -0400

    added ability to pick up coins and add it to coin total

---
+++ Assets/Scripts/Coin/CoinPickup.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+    public int coinValue = 1;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            CoinManager.instance.AddCoins(coinValue);
+            Destroy(gameObject);
+        }
+    }
+}
