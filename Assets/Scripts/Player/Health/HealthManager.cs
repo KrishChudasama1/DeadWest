2aac001d453edca4365a73dee84a9e384fc55fdc
Yue Wang <yue@dyn172-30-153-35.wireless.uwo.pri>
2026-03-23 14:15:07 -0400

    Added HealthBar, XP Bar, and XP orb

---
+++ Assets/Scripts/Player/Health/HealthManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public TMP_Text healthText;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateUI();
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateUI();
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        UpdateUI();
+    }
+
+    private void Die()
+    {
+        Debug.Log("Player died!");
+    }
+
+    public void UpdateUI()
+    {
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = currentHealth;
+        healthText.text = "HP: " + currentHealth + "/" + maxHealth;
+    }
+}
